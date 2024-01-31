364a929665386d77000003d34839c50d565a984a
Mela-nina <oge4sym@gmail.com>
2024-01-31 11:37:46 +0000

    0x01. Caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""The FIFO caching
+"""
+
+
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """The class `FIFOCache` that inherits from `BaseCaching`
+       and is a caching system
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """the `item` value for the key `key' is
+           assign to the dictionary `self.cache_data` 
+        """
+
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ This return the value in `self.cache_data` linked to `key`
+        """
+        return self.cache_data.get(key, None)
